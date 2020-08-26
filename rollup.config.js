e65bf9faea398ab26bc4e28dc53b1babe6be9bbd
Renée Kooi <renee@kooi.me>
2020-08-26 13:16:47 +0200

    Use @rollup/plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import plurals from './scripts/rollup-plugin-plurals';
 
