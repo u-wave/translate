72cb883af1713d40a437e92910e8593ab4b751a3
Renée Kooi <renee@kooi.me>
2021-07-18 13:48:37 +0200

    simplify babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,14 +1,10 @@
-module.exports = (api) => {
-  const isTesting = api.caller((caller) => caller.name === '@babel/register');
+'use strict';
 
-  return {
-    presets: [
-      ['@babel/preset-env', {
-        modules: isTesting ? 'commonjs' : false,
-        loose: true,
-        targets: isTesting ? { node: 'current' } : null,
-      }],
-    ],
-    plugins: isTesting ? ['istanbul'] : [],
-  };
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      modules: false,
+      loose: true,
+    }],
+  ],
 };
